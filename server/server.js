0a281eb1992b79b67adeae30b949424ea87e99b1
Valiukh Vlad <vladyslav.valiukh@ukr.net>
2018-05-02 19:24:32 +0300

    Vlad commit 2

--- server/server.js
+++ server/server.js
@@ -6,41 +6,14 @@ const port = 3001;
 app.listen(port, () => {
   console.log(`Server is listening on the port ${port}`);
 });
-app.get('/', (req, res) => {
-  res.send(
-pug.renderFile(
-      path.resolve(__dirname, '../pug/index.pug')
-  )
-);
-});
-app.get('/about', (req, res) => {
-  res.send(
-pug.renderFile(
-      path.resolve(__dirname, '../pug/about.pug')
-  )
-);
-});
-app.get('/gallery', (req, res) => {
-  res.send(
-pug.renderFile(
-      path.resolve(__dirname, '../pug/gallery.pug')
-  )
-);
-});
-app.get('/contacts', (req, res) => {
-  res.send(
-pug.renderFile(
-      path.resolve(__dirname, '../pug/contacts.pug')
-  )
-);
-});
-app.get('/prices', (req, res) => {
-  res.send(
-pug.renderFile(
-      path.resolve(__dirname, '../pug/prices.pug')
-  )
-);
-});
+
+var pages = ['main','gallery','contacts','prices','about']
+
+for( var i = 0 ; i < pages.length;i++){
+app.get(`/${pages[i]}`,function(req,res){
+  res.send(pug.renderFile(path.resolve(__dirname,`../pug/${req.url}.pug`)))
+});}
+
 app.use('/scss', express.static(path.resolve(__dirname, '../scss')));
 app.use('/img', express.static(path.resolve(__dirname, '../img')));
 app.use('/js', express.static(path.resolve(__dirname, '../js')));
