a4e102f91262a24b000daaa31279a45f18260f1d
Valiukh Vlad <vladyslav.valiukh@ukr.net>
2018-05-05 21:50:23 +0300

    last

--- js/shake.js
+++ js/shake.js
@@ -1,40 +1,33 @@
-$('#img1').click(function() {
-  $( '#img1' ).effect( "shake",{times:3,distance:7},1000);
+$('.one>img').hover(function() {
+  $( '.one>img' ).effect( "shake",{times:3,distance:2},1000);
 });
-$('#img1').click(function() {
-    $( '#img1' ).css( {"z-index":"100"});
-});
-$('#img2').hover(function() {
-  $( '#img2' ).effect( "shake",{times:3,distance:2},1000);
+$('.two>img').hover(function() {
+  $( '.two>img' ).effect( "shake",{times:3,distance:2},1000);
 });
 
-$('#img3').hover(function() {
-  $( '#img3' ).effect( "shake",{times:3,distance:2},1000);
+$('.three>img').hover(function() {
+  $( '.three>img' ).effect( "shake",{times:3,distance:2},1000);
 });
 
-$('#img4').hover(function() {
-  $( '#img4' ).effect( "shake",{times:2,distance:2},1000 );
+$('.four>img').hover(function() {
+  $( '.four>img' ).effect( "shake",{times:3,distance:2},1000 );
 });
-$('#img5').hover(function() {
-  $( '#img5' ).effect( "shake",{times:4,distance:2},1000 );
-});
-
-$('#img6').hover(function() {
-  $( '#img6' ).effect( "shake",{times:3,distance:2},1000 );
+$('.five>img').hover(function() {
+  $( '.five>img' ).effect( "shake",{times:3,distance:2},1000 );
 });
 
-$('#img7').hover(function() {
-  $( '#img7' ).effect( "shake",{times:3,distance:2},1000 );
+$('.six>img').hover(function() {
+  $( '.six>img' ).effect( "shake",{times:3,distance:2},1000 );
 });
 
-$('#img8').hover(function() {
-  $( '#img8' ).effect( "shake",{times:3,distance:2},1000 );
+$('.seven>img').hover(function() {
+  $( '.seven>img' ).effect( "shake",{times:3,distance:2},1000 );
 });
 
-$('#img9').hover(function() {
-  $( '#img9' ).effect( "shake",{times:3,distance:2},1000 );
+$('.eight>img').hover(function() {
+  $( '.eight>img' ).effect( "shake",{times:3,distance:2},1000 );
 });
 
-$('#img10').hover(function() {
-  $( '#img10' ).effect( "shake",{times:3,distance:2},1000 );
+$('.nine>img').hover(function() {
+  $( '.nine>img' ).effect( "shake",{times:3,distance:2},1000 );
 });
